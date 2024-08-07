319c0607b72c927e1503698f7f5dbdafcd4d3d67
Poorna chander Terala <60097572+poorna-24@users.noreply.github.com>
2024-08-07 10:43:12 +0530

    first commit

---
+++ src/redux/slices/usersSlice.js
@@ -0,0 +1,56 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import baseURL from "../../utils/baseURL";
+//initial state
+const initialState = {
+  loading: false,
+  error: null,
+  users: [],
+  user: {},
+  profile: {},
+  userAuth: {
+    loading: false,
+    error: null,
+    userInfo: {},
+  },
+};
+
+//login action
+export const loginUserAction = createAsyncThunk("users/login", async ({ email, password }, { rejectWithValue, getState, dispatch }) => {
+  try {
+    //make http req
+    const { data } = await axios.post(`${baseURL}/users/login`, {
+      email,
+      password,
+    });
+    return data;
+  } catch (error) {
+    console.log(error);
+    return rejectWithValue(error?.response?.data);
+  }
+});
+
+//users slice
+const userSlice = createSlice({
+  name: "users",
+  initialState,
+  extraReducers: (builder) => {
+    //handle actions
+    //login
+    builder.addCase(loginUserAction.pending, (state, action) => {
+      state.userAuth.loading = true;
+    });
+    builder.addCase(loginUserAction.fulfilled, (state, action) => {
+      state.userAuth.userInfo = action.payload;
+      state.userAuth.loading = false;
+    });
+    builder.addCase(loginUserAction.rejected, (state, action) => {
+      state.userAuth.error = action.payload;
+      state.userAuth.loading = false;
+    });
+  },
+});
+
+//generate reducer
+const usersReducer = userSlice.reducer;
+export default usersReducer;
---
+++ src/utils/baseURL.js
@@ -0,0 +1,4 @@
+const baseURL = "http://localhost:2030/api/v1/";
+// const baseURL = "https://e-commerce-apis-vko1.onrender.com/api/v1/";
+
+export default baseURL;
