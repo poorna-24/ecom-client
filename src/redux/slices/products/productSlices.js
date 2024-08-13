8adca589ef1fdfc634a9a4b1035cfd9f4eefff8e
Poorna chander Terala <60097572+poorna-24@users.noreply.github.com>
2024-08-13 13:14:42 +0530

    product slice

--- src/redux/slices/products/productSlices.js
+++ src/redux/slices/products/productSlices.js
@@ -1,5 +1,6 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import baseURL from "../../../utils/baseURL";
 //initial state
 const initialState = {
   products: [],
@@ -10,3 +11,50 @@ const initialState = {
   isUpdated: false,
   isDelete: false,
 };
+
+//create product action
+export const createproductAction = createAsyncThunk("product/create", async (payload, { rejectWithValue, getState, dispatch }) => {
+  try {
+    const { name, description, category, sizes, brand, colors, price } = payload;
+    //make request
+    //token-authenticated
+    //images
+    const { data } = await axios.post(`${baseURL}/products`, {
+      name,
+      description,
+      category,
+      sizes,
+      brand,
+      colors,
+      price,
+    });
+    const token = getState()?.users;
+    return data;
+  } catch (error) {
+    return rejectWithValue(error?.response?.data);
+  }
+});
+
+//slice
+const productSlice = createSlice({
+  name: "products",
+  initialState,
+  extraReducers: (builder) => {
+    //create
+    builder.addCase(createproductAction.pending, (state, action) => {
+      state.loading = false;
+      state.product = action.payload;
+      state.isAdded = true;
+    });
+    builder.addCase(createproductAction.rejected, (state, action) => {
+      state.loading = false;
+      state.product = null;
+      state.isAdded = false;
+      state.error = action.payload;
+    });
+  },
+});
+
+//generate the reducer
+const productReducer = productSlice.reducer;
+export default productReducer;
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "../slices/users/usersSlice";
+import productReducer from "../slices/products/productSlices";
 
 //store
 const store = configureStore({
   reducer: {
     users: usersReducer,
+    products: productReducer,
   },
 });
 
